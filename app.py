77e5833ccb2cdf41744f48f008644b93306640f8
CHRISTOPHER KENECHUKWU <127053076+xlinkz8@users.noreply.github.com>
2023-04-15 17:07:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -13,14 +13,16 @@ def investment():
     investor_name = input("Enter name:")
     print(investor_name , "Welcome to cohort4 Investment App")
     investor_wallet = int(input("Amount to invest:N"))
-    wallet_balance = investor_wallet * (1 + (profit_percentage / 100))
+    interest = investor_wallet * profit_percentage / 100
+wallet_balance = investor_wallet + interest 
     print("Dear Investor" , investor_name , "Your Wallet has been credited with the sum of:N" , wallet_balance, "50% interest on your capital")
     withdrawal_amount = int(input("Enter Your withdrawal Amount:N"))
     if withdrawal_amount >= wallet_balance:
-        print(investor_name,"You have insufficient Funds to perform this transaction")
+        print(investor_name,"You have an insufficient Funds to perform this transaction")
     else:
-        final_wallet_balance = wallet_balance -withdrawal_amount
-        final_balance = final_wallet_balance * (1 - (vat_percentage/100))
+        final_wallet_balance = wallet_balance - withdrawal_amount
+        final_balance = final_wallet_balance * vat_percentage/100
+final_balance = final_wallet_balance - vat_reduction
         print(investor_name ,"Your withdrawal was successful and your account has been debited with:N", withdrawal_amount, "and a 7% service charge has been deducted and" ,"your final balance is:" ,'N',final_balance)
 
 investment()
